9fcdccd55176c15f2ae0a50a32954ff373a6931c
Parunyu Pitragool <Mek@Meks-MacBook-Pro.local>
2023-12-22 14:46:01 +0700

    feat: add complete exercise

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,4 +1,21 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [messageInput, setMessageInput] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  const addMessageList = () => {
+    const newMessageList = [...messageList];
+    newMessageList.push(messageInput);
+    setMessageList(newMessageList);
+  };
+
+  const removeMessageList = (messageIndex) => {
+    const newMessageList = [...messageList];
+    newMessageList.splice(messageIndex, 1);
+    setMessageList(newMessageList);
+  };
+
   return (
     <div className="app-wrapper">
       <h1 class="app-title">Message board</h1>
@@ -9,15 +26,34 @@ function MessageBoard() {
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            onChange={(e) => {
+              setMessageInput(e.target.value);
+            }}
+            value={messageInput}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button className="submit-message-button" onClick={addMessageList}>
+          Submit
+        </button>
       </div>
       <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+        {messageList.map((message, index) => {
+          return (
+            <div className="message" key={index}>
+              <h1>{message}</h1>
+              <button
+                className="delete-button"
+                onClick={() => {
+                  {
+                    removeMessageList(index);
+                  }
+                }}
+              >
+                x
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
